d9674123328cabc18c36eac8659f60a35b2c4310
student.camm@gmail.com <student.camm@gmail.com>
2025-07-03 15:44:35 -0700

    BUILD: added index.html and app.js

---
+++ client/js/app.js
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div>
+            <h1> Auction </h1>
+        </div>
+    )
+}
+
+
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App />);
