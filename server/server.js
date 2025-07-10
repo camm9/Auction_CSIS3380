3ef4620a6542830543379cbc7117114c42c5bede
student.camm@gmail.com <student.camm@gmail.com>
2025-07-09 17:21:34 -0700

    FIXED: made key more clear for sellerDisplay name rather than createdBy

--- server/server.js
+++ server/server.js
@@ -19,29 +19,6 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
-// async function run() {
-//     try {
-//         // Connect the client to the server	(optional starting in v4.7)
-//         await client.connect();
-//         // Send a ping to confirm a successful connection
-//         await client.db("admin").command({ ping: 1 });
-//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         await client.close();
-//     }
-// }
-// run().catch(console.dir);
-
-
 async function readItems() {
     await client.connect();
     const dbName = "Auction_CSIS3380";
@@ -101,7 +78,7 @@ app.post('/create-new-listing', async (req, res) => {
         title,
         description,
         imageUrl,
-        createdBy,
+        sellerDisplayName: createdBy,
         startingBid,
         endAt,
         createdAt,
